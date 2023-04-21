080bd8a8f6a3b44b36a4f4daf2c8d6a82fc4bb4f
Martin Ueding <mu@martin-ueding.de>
2023-04-21 08:47:56 +0200

    Write test with state machine

--- brainfucktranspiler/test_codegen.py
+++ brainfucktranspiler/test_codegen.py
@@ -1,4 +1,5 @@
-from .codegen import TapeStack, op_add, op_copy
+from .codegen import TapeStack, op_add, op_copy, op_input, op_output
+from .interpreter import StateMachine
 
 
 def test_op_add() -> None:
@@ -16,3 +17,19 @@ def test_op_copy() -> None:
     destination = tape.register_variable("destination")
     code = op_copy(tape, destination, source)
     assert code == ">>[-]<<[->+>+<<]>>[-<<+>>]"
+
+
+def test_op_add_io() -> None:
+    tape = TapeStack()
+    result = tape.register_variable("result")
+    left = tape.register_variable("left")
+    right = tape.register_variable("right")
+    code = (
+        op_input(tape, left)
+        + op_input(tape, right)
+        + op_add(tape, result, left, right)
+        + op_output(tape, result)
+    )
+
+    sm = StateMachine(code, [2, 3])
+    assert sm.run() == [5]
